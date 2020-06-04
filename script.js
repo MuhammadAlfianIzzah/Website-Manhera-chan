a6b31da300bda2eedd49e50b16975b92722d8b11
unknown <alfianizzah6@gmail.com>
2020-06-04 21:40:29 +0800

    MENAMBAHKAN FITUR GALERY

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ burger.addEventListener('click', function () {
     this.classList.toggle('click');
     li.classList.toggle('active');
 });
+
+let galery = document.querySelector('.galery');
+let primary = document.querySelector('.primary img');
+
+galery.addEventListener('click', function (e) {
+    let imgTarget = e.target;
+    let imgGalery = document.querySelectorAll('.imgGalery');
+    let primary = document.querySelector('.imgPrimary');
+    // console.log(primary);
+    imgTarget.classList.add('fadein');
+    setTimeout(() => {
+        imgTarget.classList.remove('fadein');
+    }, 500);
+    imgGalery.forEach((e) => {
+
+        if (e.classList.contains('active')) {
+            e.classList.remove('active');
+        }
+    });
+    if (imgTarget.classList.contains('imgGalery')) {
+        imgTarget.classList.add('active');
+        primary.src = imgTarget.src;
+    }
+
+    primary.classList = "imgPrimary";
+
+});
